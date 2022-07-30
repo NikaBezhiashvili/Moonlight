f94d5bce46e52eaf93b447a1f747338a16ab9684
NikaBezhiashvili <74880697+NikaBezhiashvili@users.noreply.github.com>
2022-07-30 13:39:12 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+
+let stars = document.getElementById('stars');
+
+let moon = document.getElementById('moon');
+
+let mountains_behind = document.getElementById('mountains_behind');
+
+let mountains_front = document.getElementById('mountains_front');
+
+let text = document.getElementById('text');
+
+let btn = document.getElementById('btn');
+
+let header = document.querySelector('header')
+
+window.addEventListener('scroll', function(){
+    let value = window.scrollY;
+    stars.style.left = value + 'px';
+    moon.style.top =  value + 'px';
+    mountains_behind.style.top = value / 2 + 'px';
+    text.style.marginRight = value * 3 + 'px';
+    text.style.marginTop = value * 1.5 + 'px';
+    btn.style.marginTop = value * 1.5 + 'px';
+    header.style.top = value / 2 + 'px';
+})
